7500cbe2296fd6b37e809710aa808b2c3919b2d1
Ben Schoelkopf <thebenschoel@gmail.com>
2023-12-06 10:23:24 -0500

    stuff

---
+++ SuperAuth.js
@@ -0,0 +1,36 @@
+const SuperAuth = function () {
+    this.evaluate = function (context) {
+        const http_request = new NetworkHTTPRequest();
+
+        http_request.requestUrl =
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.firebaseApiKey;
+        http_request.requestMethod = 'POST';
+        http_request.requestBody = JSON.stringify({
+            email: this.email,
+            password: this.password,
+            returnSecureToken: true,
+        });
+        http_request.setRequestHeader('Referer', 'http://localhost:3000');
+        http_request.setRequestHeader('Content-Type', 'application/json');
+        http_request.send();
+
+        return JSON.parse(http_request.responseBody)["idToken"];
+    };
+};
+
+SuperAuth.identifier = 'com.eulerity.SuperAuth';
+SuperAuth.title = 'SuperAuth';
+SuperAuth.help = 'https://eulerity.com';
+
+// AIzaSyBNW2VNKcSzm5DqMXm0HSgdRVAS2-8470Q
+
+SuperAuth.inputs = [
+    InputField('email', 'Email', 'String'),
+    InputField('password', 'Password', 'SecureValue'),
+    InputField('firebaseApiKey', 'Firebase API Key', 'String', {
+        persisted: true,
+        defaultValue: 'AIzaSyBNW2VNKcSzm5DqMXm0HSgdRVAS2-8470Q',
+    })
+];
+
+registerDynamicValueClass(SuperAuth);
